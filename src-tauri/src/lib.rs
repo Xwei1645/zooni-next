11573a7885e534b18f2ee383ec5d9f106023bf5c
Wei Wu <xuwei1645@163.com>
2026-07-22 17:43:44 +0800

    chore: remove tauri-plugin-opener and enhance security

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,23 +2,15 @@ mod fonts;
 mod settings;
 mod windows;
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_opener::init())
         .manage(windows::OptionsWindowState::default())
         .setup(|app| {
             windows::preload_options_window(&app.handle())?;
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
-            greet,
             fonts::list_system_fonts,
             settings::get_app_settings,
             settings::save_app_settings,
